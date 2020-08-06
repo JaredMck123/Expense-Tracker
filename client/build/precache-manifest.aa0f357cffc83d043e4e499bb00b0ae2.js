6bc0e43ada81cc2363fe0c774c4a73f8ee43c98f
Jared McKinney <jaredmckinney@Jareds-MacBook-Pro.local>
2020-08-06 14:18:18 -0400

    first commit

---
+++ client/build/precache-manifest.aa0f357cffc83d043e4e499bb00b0ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a993d6d3edcf433342b3ec50695f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6e71233a2256eab1910",
+    "url": "/static/css/main.df8c2e55.chunk.css"
+  },
+  {
+    "revision": "f35734120bbada6b5fb5",
+    "url": "/static/js/2.21524b19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.21524b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e71233a2256eab1910",
+    "url": "/static/js/main.f3db9bd8.chunk.js"
+  },
+  {
+    "revision": "9101dc49b098f862f059",
+    "url": "/static/js/runtime-main.21dc3bc9.js"
+  }
+]);
